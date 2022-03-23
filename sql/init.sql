9362390e2bd3acc9750d23eaa76ac9acf9dec133
bbradforddesign <bbradforddesign@gmail.com>
2022-03-23 15:37:44 -0500

    remove test schema from init script

--- sql/init.sql
+++ sql/init.sql
@@ -1,18 +1,8 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 SELECT uuid_generate_v1();
 
-CREATE SCHEMA IF NOT EXISTS test;
-
-    CREATE TABLE test.users (
-        id uuid DEFAULT uuid_generate_v1 (),
-        first_name text,
-        last_name text
-    );
-
-CREATE SCHEMA IF NOT EXISTS dev;
-
-    CREATE TABLE dev.users (
-        id uuid DEFAULT uuid_generate_v1 (),
-        first_name text,
-        last_name text
-    );
+CREATE TABLE user_account (
+    id uuid DEFAULT uuid_generate_v1 (),
+    first_name text,
+    last_name text
+);
