f61b4042013fbbe9ea53341f8c8da23b26aaf11f
bbradforddesign <bbradforddesign@gmail.com>
2022-03-28 20:49:47 -0500

    moved business logic out of router

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,19 +1,23 @@
 import { User } from "../interfaces/user";
 import UserModel from "../models/user";
+import { ApiError } from "../classes/apiError";
 
 const UserController = {
-    // registers user within postgres if already within Auth0
-    async registerUser(fields: User): Promise<User> {
-        try {
-            const res = await UserModel.create(fields);
-            return res.rows[0];
-        } catch (err) {
-            throw err;
-        }
-    },
     async getUser(id: string): Promise<User> {
         try {
-            const res = await UserModel.get(id);
+            // if no rows found, create new blank profile
+            let res = await UserModel.get(id);
+
+            if (!res || res.rows.length === 0) {
+                res = await UserModel.create({
+                    id: id,
+                });
+
+                if (!res || res.rows.length === 0) {
+                    throw new ApiError(500, "failed to retrieve user");
+                }
+            }
+
             return res.rows[0];
         } catch (err) {
             throw err;
@@ -21,7 +25,13 @@ const UserController = {
     },
     async updateUser(user: User): Promise<User> {
         try {
+            // upsert user profile with supplied fields
             const res = await UserModel.update(user);
+
+            if (!res || res.rows.length === 0) {
+                throw new ApiError(500, "failed to update user");
+            }
+
             return res.rows[0];
         } catch (err) {
             throw err;
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -20,10 +20,6 @@ userRouter.put("/profile", async (req, res, next) => {
             last_name: fields.last_name,
         });
 
-        if (!updatedProfile) {
-            throw new ApiError(500, "failed to update user");
-        }
-
         return res.status(200).json(updatedProfile);
     } catch (err) {
         return next(err);
@@ -35,21 +31,8 @@ userRouter.get("/profile", async (req, res, next) => {
         return next(new ApiError(401, "unauthorized"));
     }
 
-    // TODO: since controller methods currently map 1:1 to model,
-    // no reason for controller currently.
-    // need to move business logic into controller.
     try {
-        // retrieve user from db
-        let user = await UserController.getUser(req.user.sub);
-        if (!user) {
-            // if no match, create new profile from ID token
-            user = await UserController.registerUser({
-                id: req.user.sub,
-            });
-            if (!user) {
-                throw new ApiError(500, "failed to register user");
-            }
-        }
+        const user = await UserController.getUser(req.user.sub);
 
         return res.status(200).json(user);
     } catch (err) {
